cee438d72962649f22abcaa0cc965cefd8f2fb18
mjbutron <butron87@gmail.com>
2019-09-25 20:02:57 +0200

    Añadido Access Control a las rutas
    Co-Authored-By: null <mjbutron@users.noreply.github.com>

--- public/index.php
+++ public/index.php
@@ -11,4 +11,17 @@ $app = new \Slim\App;
 require_once('../src/routes/service.php');
 require_once('../src/routes/slider.php');
 
+// Add access control
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'http://localhost:4200') // TODO cambiar por url origen
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE,');
+});
+
 $app->run();
