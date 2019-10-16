4282ee1fa8efb0c509cf665231b89dfb4d85c77a
mjbutron <butron87@gmail.com>
2019-10-16 20:56:52 +0200

    Añadido código error login
    Co-Authored-By: null <mjbutron@users.noreply.github.com>

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use \Firebase\JWT\JWT;
 $app->post('/login', function (Request $request, Response $response, array $args) {
 
     $input = $request->getParsedBody();
-    $sql = "SELECT * FROM user WHERE email= :email";
+    $sql = "SELECT id, name, surname, email, telephone, password FROM user WHERE email= :email";
     $sth = $this->db->prepare($sql);
     $sth->bindParam("email", $input['email']);
     $sth->execute();
@@ -15,18 +15,22 @@ $app->post('/login', function (Request $request, Response $response, array $args
 
     // verify email address.
     if(!$user) {
-        return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);
+        return $this->response->withStatus(400)
+        ->withHeader('Content-Type', 'application/json')
+        ->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);
     }
 
     // verify password.
     if (!password_verify($input['password'],$user->password)) {
-        return $this->response->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);
+      return $this->response->withStatus(400)
+      ->withHeader('Content-Type', 'application/json')
+      ->withJson(['error' => true, 'message' => 'These credentials do not match our records.']);
     }
 
     $settings = $this->get('settings'); // get settings array.
 
     $token = JWT::encode(['id' => $user->id, 'email' => $user->email], $settings['jwt']['secret'], "HS256");
-
+    $user->password = "";
     return $this->response->withJson(['token' => $token, 'user' => $user]);
 
 });
