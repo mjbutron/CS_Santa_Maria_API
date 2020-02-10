aaa496d0fdf96f494cfb31f8aa7a63c6cd403173
mjbutron <butron87@gmail.com>
2020-02-10 18:24:42 +0100

    Añadido servicio para crear nuevo taller

--- src/workshop.php
+++ src/workshop.php
@@ -53,41 +53,75 @@ $app->get('/api/workshopsByPage/{page}', function(Request $request, Response $re
   }
 });
 
-//
-// // POST: Add new service
-// $app->post('/admin/api/service/new', function(Request $request, Response $response, array $args){
-//    $title = $request->getParam('title');
-//    $image_home = $request->getParam('image_home');
-//    $subtitle_home = $request->getParam('subtitle_home');
-//    $description_home = $request->getParam('description_home');
-//    $home = $request->getParam('home');
-//    $image = $request->getParam('image');
-//    $subtitle = $request->getParam('subtitle');
-//    $description = $request->getParam('description');
-//    $user = $request->getParam('user');
-//
-//   $sql = "INSERT INTO service (title, image_home, subtitle_home, description_home, home, image, subtitle, description, create_date, update_date, user_id)
-//   VALUES (:title, :image_home, :subtitle_home, :description_home, :home, :image, :subtitle, :description,  CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP(), :user)";
-//
-//   try{
-//     $res = $this->db->prepare($sql);
-//     $res->bindParam(':title', $title);
-//     $res->bindParam(':image_home', $image_home);
-//     $res->bindParam(':subtitle_home', $subtitle_home);
-//     $res->bindParam(':description_home', $description_home);
-//     $res->bindParam(':home', $home);
-//     $res->bindParam(':image', $image);
-//     $res->bindParam(':subtitle', $subtitle);
-//     $res->bindParam(':description', $description);
-//     $res->bindParam(':user', $user);
-//     $res->execute();
-//     return $this->response->withJson(['cod' => '200', 'message' => 'Nuevo servicio creado.']);
-//     $res = null;
-//   }catch(PDOException $e){
-//     echo '{"error" : {"text":'.$e->getMessage().'}';
-//   }
-// });
-//
+
+// POST: Add new workshops
+$app->post('/admin/api/workshops/new', function(Request $request, Response $response, array $args){
+  $home = $request->getParam('home');
+  $title = $request->getParam('title');
+  $description_home = $request->getParam('description_home');
+  $image = $request->getParam('image');
+  $subtitle = $request->getParam('subtitle');
+  $price = $request->getParam('price');
+  $address = $request->getParam('address');
+  $session_date = $request->getParam('session_date');
+  $session_start = $request->getParam('session_start');
+  $session_end = $request->getParam('session_end');
+  $sessions = $request->getParam('sessions');
+  $description = $request->getParam('description');
+  $user_id = $request->getParam('user_id');
+
+  $sql = "INSERT INTO workshop (
+            home,
+            title,
+            description_home,
+            image,
+            subtitle,
+            price,
+            address,
+            session_date,
+            session_start,
+            session_end,
+            sessions,
+            description,
+            user_id)
+          VALUES (
+            :home,
+            :title,
+            :description_home,
+            :image,
+            :subtitle,
+            :price,
+            :address,
+            :session_date,
+            :session_start,
+            :session_end,
+            :sessions,
+            :description,
+            :user_id)";
+
+  try{
+    $res = $this->db->prepare($sql);
+    $res->bindParam(':home', $home);
+    $res->bindParam(':title', $title);
+    $res->bindParam(':description_home', $description_home);
+    $res->bindParam(':image', $image);
+    $res->bindParam(':subtitle', $subtitle);
+    $res->bindParam(':price', $price);
+    $res->bindParam(':address', $address);
+    $res->bindParam(':session_date', $session_date);
+    $res->bindParam(':session_start', $session_start);
+    $res->bindParam(':session_end', $session_end);
+    $res->bindParam(':sessions', $sessions);
+    $res->bindParam(':description', $description);
+    $res->bindParam(':user_id', $user_id);
+    $res->execute();
+    return $this->response->withJson(['cod' => '200', 'message' => 'Nuevo taller creado.']);
+    $res = null;
+  }catch(PDOException $e){
+    echo '{"error" : {"text":'.$e->getMessage().'}';
+  }
+});
+
 // PUT: Update workshops
 $app->put('/admin/api/workshops/update/{id}', function(Request $request, Response $response, array $args){
    $id_workshop = $request->getAttribute('id');
