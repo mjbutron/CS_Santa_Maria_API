3020cc4186830daf4b2a2df326445f30c9bafa19
mjbutron <butron87@gmail.com>
2020-02-19 19:40:20 +0100

    Añadido CRUD para servicios

--- src/service.php
+++ src/service.php
@@ -25,33 +25,64 @@ $app->get('/api/allServices', function(Request $request, Response $response, arr
   }
 });
 
+// GET: Get services by page
+$app->get('/api/servicesByPage/{page}', function(Request $request, Response $response, array $args){
+  $page = $request->getAttribute('page');
+  $resultPerPage = 5;
+  $start = ($page - 1) * $resultPerPage;
+
+  $sql = "SELECT COUNT(*) FROM service";
+  $sqlPage = "SELECT * FROM service ORDER BY create_date ASC LIMIT $start, $resultPerPage";
+  try{
+    $res = $this->db->prepare($sql);
+    $res->execute();
+    $count = $res->fetchColumn();
+
+    if($count) {
+      $res = $this->db->prepare($sqlPage);
+      $res->execute();
+      $workshops = $res->fetchAll(PDO::FETCH_OBJ);
+      return $this->response->withJson(['allServices' => $workshops, 'total' => $count, 'actual' => $page, 'totalPages' => ceil($count/$resultPerPage)]);
+    }else{
+      return $this->response->withJson(['cod' => '404', 'message' => 'Datos no disponibles.']);
+    }
+
+    $res = null;
+  }catch(PDOException $e){
+    echo '{"error" : {"text":'.$e->getMessage().'}';
+  }
+});
 
 // POST: Add new service
-$app->post('/admin/api/service/new', function(Request $request, Response $response, array $args){
+$app->post('/admin/api/services/new', function(Request $request, Response $response, array $args){
    $title = $request->getParam('title');
-   $image_home = $request->getParam('image_home');
-   $subtitle_home = $request->getParam('subtitle_home');
-   $description_home = $request->getParam('description_home');
-   $home = $request->getParam('home');
    $image = $request->getParam('image');
    $subtitle = $request->getParam('subtitle');
    $description = $request->getParam('description');
-   $user = $request->getParam('user');
+   $user_id = $request->getParam('user_id');
 
-  $sql = "INSERT INTO service (title, image_home, subtitle_home, description_home, home, image, subtitle, description, create_date, update_date, user_id)
-  VALUES (:title, :image_home, :subtitle_home, :description_home, :home, :image, :subtitle, :description,  CURRENT_TIMESTAMP(),  CURRENT_TIMESTAMP(), :user)";
+
+
+  $sql = "INSERT INTO service (
+            title,
+            image,
+            subtitle,
+            description,
+            user_id)
+          VALUES (
+            :title,
+            :image,
+            :subtitle,
+            :description,
+            :user_id)";
 
   try{
     $res = $this->db->prepare($sql);
     $res->bindParam(':title', $title);
-    $res->bindParam(':image_home', $image_home);
-    $res->bindParam(':subtitle_home', $subtitle_home);
-    $res->bindParam(':description_home', $description_home);
-    $res->bindParam(':home', $home);
     $res->bindParam(':image', $image);
     $res->bindParam(':subtitle', $subtitle);
     $res->bindParam(':description', $description);
-    $res->bindParam(':user', $user);
+    $res->bindParam(':user_id', $user_id);
     $res->execute();
     return $this->response->withJson(['cod' => '200', 'message' => 'Nuevo servicio creado.']);
     $res = null;
@@ -60,44 +91,48 @@ $app->post('/admin/api/service/new', function(Request $request, Response $respon
   }
 });
 
-// PUT: Update service
-$app->put('/admin/api/service/update/{id}', function(Request $request, Response $response, array $args){
+// PUT: Update service by ID
+$app->put('/admin/api/services/update/{id}', function(Request $request, Response $response, array $args){
    $id_service = $request->getAttribute('id');
    $title = $request->getParam('title');
-   $image_home = $request->getParam('image_home');
-   $subtitle_home = $request->getParam('subtitle_home');
-   $description_home = $request->getParam('description_home');
-   $home = $request->getParam('home');
    $image = $request->getParam('image');
    $subtitle = $request->getParam('subtitle');
    $description = $request->getParam('description');
-   $user = $request->getParam('user');
+   $user_id = $request->getParam('user_id');
 
   $sql= "UPDATE service SET
           title = :title,
-          image_home = :image_home,
-          subtitle_home = :subtitle_home,
-          description_home = :description_home,
-          home = :home,
           image = :image,
           subtitle = :subtitle,
           description = :description,
-          user_id = :user
+          user_id = :user_id
           WHERE id = $id_service";
 
   try{
     $res = $this->db->prepare($sql);
     $res->bindParam(':title', $title);
-    $res->bindParam(':image_home', $image_home);
-    $res->bindParam(':subtitle_home', $subtitle_home);
-    $res->bindParam(':description_home', $description_home);
-    $res->bindParam(':home', $home);
     $res->bindParam(':image', $image);
     $res->bindParam(':subtitle', $subtitle);
     $res->bindParam(':description', $description);
-    $res->bindParam(':user', $user);
+    $res->bindParam(':user_id', $user_id);
     $res->execute();
     return $this->response->withJson(['cod' => '200', 'message' => 'Servicio actualizado.']);
+    $res = null;
+  }catch(PDOException $e){
+    echo '{"error" : {"text":'.$e->getMessage().'}';
+  }
+});
+
+// DELETE: Delete service by ID
+$app->delete('/admin/api/services/delete/{id}', function(Request $request, Response $response, array $args){
+  $id_service = $request->getAttribute('id');
+  $sql = "DELETE FROM service WHERE id = $id_service";
+  try{
+    $res = $this->db->prepare($sql);
+    $res->bindParam(':id', $id_service);
+    $res->execute();
+    return $this->response->withJson(['cod' => '200', 'message' => 'Servicio eliminado.']);
+
     $res = null;
   }catch(PDOException $e){
     echo '{"error" : {"text":'.$e->getMessage().'}';
