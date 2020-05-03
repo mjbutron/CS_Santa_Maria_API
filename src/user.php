66f900774680a674a371d0b082f1f3f0b9d0bc30
mjbutron <butron87@gmail.com>
2020-05-03 18:36:36 +0200

    Añadido nuevos atributos al usuario

--- src/user.php
+++ src/user.php
@@ -5,8 +5,9 @@ use \Firebase\JWT\JWT;
 
 // Routes User
 
-// GET: Get user
-$app->get('/admin/api/userAccount', function(Request $request, Response $response, array $args){
+// POST: Get user profile
+$app->post('/admin/api/userProfile', function(Request $request, Response $response, array $args){
+  $input = $request->getParsedBody();
   $sql = "SELECT * FROM user WHERE email= :email";
   try{
     $input = $request->getParsedBody();
@@ -21,6 +22,47 @@ $app->get('/admin/api/userAccount', function(Request $request, Response $respons
       return $this->response->withJson(['cod' => '404', 'message' => 'El usuario no existe.']);
     }
 
+    $res = null;
+  }catch(PDOException $e){
+    echo '{"error" : {"text":'.$e->getMessage().'}';
+  }
+});
+
+// PUT: Update user profile
+$app->put('/admin/api/userProfile/update/{id}', function(Request $request, Response $response, array $args){
+   $id = $request->getAttribute('id');
+   $name = $request->getParam('name');
+   $surname = $request->getParam('surname');
+   $telephone = $request->getParam('telephone');
+   $address = $request->getParam('address');
+   $city = $request->getParam('city');
+   $province = $request->getParam('province');
+   $zipcode = $request->getParam('zipcode');
+   $aboutme = $request->getParam('aboutme');
+
+  $sql = "UPDATE user SET
+          name = :name,
+          surname = :surname,
+          telephone = :telephone,
+          address = :address,
+          city = :city,
+          province = :province,
+          zipcode = :zipcode,
+          aboutme = :aboutme
+        WHERE id = $id";
+
+  try{
+    $res = $this->db->prepare($sql);
+    $res->bindParam(':name', $name);
+    $res->bindParam(':surname', $surname);
+    $res->bindParam(':telephone', $telephone);
+    $res->bindParam(':address', $address);
+    $res->bindParam(':city', $city);
+    $res->bindParam(':province', $province);
+    $res->bindParam(':zipcode', $zipcode);
+    $res->bindParam(':aboutme', $aboutme);
+    $res->execute();
+    return $this->response->withJson(['cod' => '200', 'message' => 'Información actualizada.']);
     $res = null;
   }catch(PDOException $e){
     echo '{"error" : {"text":'.$e->getMessage().'}';
