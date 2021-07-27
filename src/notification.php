dc97dca5b11d37ca82943d0ba4ee50557e2108b1
mjbutron <butron87@gmail.com>
2021-07-27 19:18:15 +0200

    Añadido Notificaciones (parte 2)

--- src/notification.php
+++ src/notification.php
@@ -5,8 +5,8 @@ use \Firebase\JWT\JWT;
 
 // Routes Notification
 
-// GET: find notification
-$app->get('/admin/api/findNotifications', function(Request $request, Response $response, array $args){
+// POST: find notification
+$app->post('/admin/api/findNotifications', function(Request $request, Response $response, array $args){
   $sql = "SELECT COUNT(id)
   FROM notification
   WHERE user_id=:user_id AND notified = 0";
@@ -26,28 +26,7 @@ $app->get('/admin/api/findNotifications', function(Request $request, Response $r
   }
 });
 
-// GET: Get all notifications
-$app->get('/admin/api/notifications', function(Request $request, Response $response, array $args){
-  $sql = "SELECT * FROM notification
-  WHERE user_id= :user_id
-  ORDER BY datetime_notification DESC";
-  try{
-    $input = $request->getParsedBody();
-    $res = $this->db->prepare($sql);
-    $res->bindParam("user_id", $input['user_id']);
-    $res->execute();
-    $notifications = $res->fetchAll(PDO::FETCH_OBJ);
-
-    return $this->response->withJson(['cod' => '200', 'allNotifications' => $notifications]);
-    $res = null;
-
-  }catch(PDOException $e){
-    return $this->response->withStatus(503)->withHeader('Content-Type', 'application/json')
-    ->withJson(['cod' => 503, 'message' => 'No es posible conectar con la base de datos.']);
-  }
-});
-
-// GET: Get all notifications by page
+// POST: Get all notifications by page
 $app->post('/admin/api/notificationsByPage/{page}', function(Request $request, Response $response, array $args){
   $page = $request->getAttribute('page');
   $resultPerPage = 5;
@@ -69,6 +48,24 @@ $app->post('/admin/api/notificationsByPage/{page}', function(Request $request, R
     $res->execute();
     $notifications = $res->fetchAll(PDO::FETCH_OBJ);
     return $this->response->withJson(['cod' => '200', 'allNotifications' => $notifications, 'total' => $count, 'actual' => $page, 'totalPages' => ceil($count/$resultPerPage)]);
+    $res = null;
+
+  }catch(PDOException $e){
+    return $this->response->withStatus(503)->withHeader('Content-Type', 'application/json')
+    ->withJson(['cod' => 503, 'message' => 'No es posible conectar con la base de datos.']);
+  }
+});
+
+// PUT: Update notified
+$app->put('/admin/api/allNotified', function(Request $request, Response $response, array $args){
+  $user_id = $request->getParam('user_id');
+  $sql= "UPDATE notification SET
+          notified = 1
+          WHERE user_id = $user_id";
+  try{
+    $res = $this->db->prepare($sql);
+    $res->execute();
+    return $this->response->withJson(['cod' => '200', 'message' => 'Se han notificado.']);
     $res = null;
 
   }catch(PDOException $e){
