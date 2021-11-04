09530ead8601a3261e61f6ef47e3486d257c0043
mjbutron <butron87@gmail.com>
2021-11-04 20:41:15 +0100

    Envió de email

--- src/sendemail.php
+++ src/sendemail.php
@@ -8,15 +8,19 @@ use \Firebase\JWT\JWT;
 // POST: Send email
 $app->post('/api/sendEmail', function(Request $request, Response $response, array $args){
 
-  $to = $request->getParam('to');
+  $to = "butron87@gmail.com"; // Email del centro
+  $name = $request->getParam('name');
+  $surname = $request->getParam('surname');
+  $email = $request->getParam('email');
   $subject = $request->getParam('subject');
   $message = $request->getParam('message');
+  $content = "Nombre: " . $name . "\nApellidos: " . $surname . "\nEmail: " . $email . "\n\nMensaje:\n" . $message . "\n\nEmail de contacto enviado desde la web del Centro Sanitario Santa María.";
 
-  // $to = "destinatario@email.com, destinatario2@email.com, destinatario3@email.com";
-  // $subject = "Asunto del email";
-  // $message = "Este es mi primer envío de email con PHP";
-  // $headers = "From: mi@cuentadeemail.com" . "\r\n" . "CC: destinatarioencopia@email.com";
-
-mail($to, $subject, $message, $headers);
-
+  if(mail($to, $subject, $content)){
+    return $this->response->withJson(['cod' => '200', 'message' => 'Email enviado']);
+  }
+  else {
+    return $this->response->withStatus(503)->withHeader('Content-Type', 'application/json')
+    ->withJson(['cod' => 503, 'message' => $e]);
+  }
 });
