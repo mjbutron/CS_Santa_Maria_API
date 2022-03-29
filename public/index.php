2a5bd44ad7086911f67525792aa0a21be8f007a0
mjbutron <butron87@gmail.com>
2022-03-29 21:27:50 +0200

    Reestructuración de rutas

--- public/index.php
+++ public/index.php
@@ -12,23 +12,10 @@ require __DIR__ . '/../src/configuration/db.php';
 require __DIR__ . '/../src/handlers/handlers.php';
 // Register middleware configuration
 require __DIR__ . '/../src/middleware/middleware.php';
+// Register services
+require __DIR__ . '/../src/services/upload.php';
 // Register routes
-require __DIR__ . '/../src/access.php';
-require __DIR__ . '/../src/slider.php';
-require __DIR__ . '/../src/service.php';
-require __DIR__ . '/../src/upload.php';
-require __DIR__ . '/../src/user.php';
-require __DIR__ . '/../src/workshop.php';
-require __DIR__ . '/../src/course.php';
-require __DIR__ . '/../src/homeinfo.php';
-require __DIR__ . '/../src/footerinfo.php';
-require __DIR__ . '/../src/contact.php';
-require __DIR__ . '/../src/opinion.php';
-require __DIR__ . '/../src/aboutus.php';
-require __DIR__ . '/../src/roles.php';
-require __DIR__ . '/../src/users.php';
-require __DIR__ . '/../src/notification.php';
-require __DIR__ . '/../src/sendemail.php';
+require __DIR__ . '/../src/routes/routes_config.php';
 
 $app->run();
 
---
+++ src/routes/routes_config.php
@@ -0,0 +1,19 @@
+<?php
+// Register routes
+require __DIR__ . '/access.php';
+require __DIR__ . '/slider.php';
+require __DIR__ . '/service.php';
+require __DIR__ . '/user.php';
+require __DIR__ . '/workshop.php';
+require __DIR__ . '/course.php';
+require __DIR__ . '/homeinfo.php';
+require __DIR__ . '/footerinfo.php';
+require __DIR__ . '/contact.php';
+require __DIR__ . '/opinion.php';
+require __DIR__ . '/aboutus.php';
+require __DIR__ . '/roles.php';
+require __DIR__ . '/users.php';
+require __DIR__ . '/notification.php';
+require __DIR__ . '/sendemail.php';
+
+?>
