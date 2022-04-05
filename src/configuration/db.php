fd48f56dd06f3e75b5010ec92b81a1a4d8eba58f
mjbutron <butron87@gmail.com>
2022-04-05 20:48:03 +0200

    try/cath en conexión a base de datos

--- src/configuration/db.php
+++ src/configuration/db.php
@@ -2,12 +2,16 @@
 // Configuration Data Base
 
 // DB instance
-$container['db'] = function ($c) {
-    $db = $c['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['name'] . ";charset=utf8", $db['user'], $db['password']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $pdo;
-};
+try {
+  $container['db'] = function ($c) {
+      $db = $c['settings']['db'];
+      $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['name'] . ";charset=utf8", $db['user'], $db['password']);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+      return $pdo;
+  };
+} catch (PDOException $e){
+    return $e->getMessage();
+}
 
 ?>
